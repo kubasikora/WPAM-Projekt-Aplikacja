c52e493037a8cb3616f3eaded488a75a91713597
kubasikora <283418@pw.edu.pl>
2021-01-05 13:47:10 +0100

    settings layout

--- app/src/main/java/pw/wpam/polityper/fragments/SettingsFragment.kt
+++ app/src/main/java/pw/wpam/polityper/fragments/SettingsFragment.kt
@@ -1,15 +1,13 @@
 package pw.wpam.polityper.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 import pw.wpam.polityper.R
 import pw.wpam.polityper.viewmodels.UserViewModel
@@ -22,6 +20,9 @@ class SettingsFragment : Fragment() {
         view.logoutButton.setOnClickListener {
             userViewModel.logout()
         }
+        userViewModel.currentUser.observe(this.viewLifecycleOwner, Observer { user ->
+            view.findViewById<TextView>(R.id.usernameProfile).text = "Hey ${user?.username}!"
+        })
         return view
     }
 }
