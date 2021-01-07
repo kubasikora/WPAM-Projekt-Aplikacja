ed93a108c85a2c21bf1ae2097ed8096f8a28fb5d
kubasikora <283418@pw.edu.pl>
2021-01-07 17:41:15 +0100

    working map view

--- app/src/main/java/pw/wpam/polityper/fragments/MatchDetailFragment.kt
+++ app/src/main/java/pw/wpam/polityper/fragments/MatchDetailFragment.kt
@@ -8,6 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.view.isVisible
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.findFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.fragment_match_detail.*
 import kotlinx.android.synthetic.main.fragment_match_detail.view.*
 import pw.wpam.polityper.R
@@ -15,9 +23,18 @@ import pw.wpam.polityper.adapters.ViewPageAdapter
 import pw.wpam.polityper.services.BetService
 import pw.wpam.polityper.models.MatchStats
 
-class MatchDetailFragment : Fragment() {
+class MatchDetailFragment : Fragment(), OnMapReadyCallback {
+    private var mMap: GoogleMap? = null
+    private var mStats: MatchStats? = null
+    private var mapSet: Boolean = false
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_match_detail, container, false)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map)!! as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        view.map.isVisible = false
+
         val matchId = this.arguments?.getInt("matchId")
 
         val playerOnePagerAdapter = ViewPageAdapter(this.requireActivity())
@@ -30,12 +47,37 @@ class MatchDetailFragment : Fragment() {
 
         view.loadingSpinner.isVisible = true
         BetService.getMatchStatistics(matchId!!) { success, stats: MatchStats? ->
+            mStats = stats!!
+
             view.loadingSpinner.isVisible = false
+            view.map.isVisible = true
+
             view.findViewById<TextView>(R.id.statsPlayerOne).text = stats?.playerOne?.name
             view.findViewById<TextView>(R.id.statsPlayerTwo).text = stats?.playerTwo?.name
             playerOnePagerAdapter.update(stats?.playerOneForm!!)
             playerTwoPagerAdapter.update(stats?.playerTwoForm!!)
+
+            if(mMap != null && !mapSet){
+                mapSet = true
+                val position = LatLng(stats?.venue?.latitude!!, stats?.venue?.longitude!!)
+                Log.d("MAPxd", mStats?.venue?.name!!)
+                val marker = mMap?.addMarker(MarkerOptions().position(position).title(stats?.venue?.name!!))
+                marker?.showInfoWindow()
+                mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 17.0f))
+            }
         }
         return view
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        mMap = p0!!
+        if(mStats != null && !mapSet){
+            mapSet = true
+            val position = LatLng(mStats?.venue?.latitude!!, mStats?.venue?.longitude!!)
+            Log.d("MAP", mStats?.venue?.name!!)
+            val marker = mMap?.addMarker(MarkerOptions().position(position).title(mStats?.venue?.name!!))
+            marker?.showInfoWindow()
+            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 15.0f))
+        }
     }
 }
--- app/src/main/java/pw/wpam/polityper/models/Venue.kt
+++ app/src/main/java/pw/wpam/polityper/models/Venue.kt
@@ -1,5 +1,8 @@
 package pw.wpam.polityper.models
 
 data class Venue(
-        val id: Int
+        val id: Int,
+        val name: String,
+        val latitude: Double,
+        val longitude: Double
 )
